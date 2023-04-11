9156ea197f8660c708843e6fe95c3e87fe2a2de2
William Poletto <poletto123@gmail.com>
2023-04-11 18:44:13 -0300

    Adjusting CSS

--- v2/src/components/Common.js
+++ v2/src/components/Common.js
@@ -4,8 +4,8 @@ import { Flex, Text, Box } from '@chakra-ui/react';
 const Common = () => {
 
   return (
-    <Flex justify="center" align="center" height="15vh" width="80%" margin="0 auto" marginBottom="0" borderStyle="solid" borderRadius="20px">
-        <Box width="99%" padding="0" backgroundColor="#00022C" >
+    <Flex justify="center" align="center" width="80%" margin="0 auto" borderStyle="solid" borderRadius="20px">
+        <Box width="99%" borderRadius="20px" backgroundColor="#00022C" >
             <Text fontSize="30px">Lightsaber Cat NFT</Text>
             <Text fontSize="20px">Living in his castle, he contemplates on the smallness of humans, and uses the Force to protect his cat realm</Text>
         </Box>
